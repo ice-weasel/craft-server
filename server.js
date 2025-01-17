ac2794c72638cf0b7f2e566e4f9812d94b556710
Abhishek <abhishek.raman018@gmail.com>
2025-01-17 22:30:38 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer(function(req,res) {
+    res.writeHead(200, { 'Content-Type' : 'text/html' })
+    res.write('Hello Node')
+    res.end()
+})
+
+server.listen(port, function(error) {
+    if(error){
+        console.log("some error",error);
+    }
+    else {
+        console.log("server listening ",port);
+    }
+})
